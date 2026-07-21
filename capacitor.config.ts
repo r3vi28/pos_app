84c381a11062cc783ed1d4ad1d360e30107f53e7
Moises Javier Revi <moisesjr3vi@gmail.com>
2026-07-21 19:00:03 -0400

    Initial Ionic project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pos-app',
+  webDir: 'www'
+};
+
+export default config;
